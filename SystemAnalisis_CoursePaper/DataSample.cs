f3fe2a1ed961f6df24ab18a9c036f10182fe3607
atmay <liubov.burtseva@gmail.com>
2022-01-11 07:53:12 +0300

    Add project files.

---
+++ SystemAnalisis_CoursePaper/DataSample.cs
@@ -0,0 +1,116 @@
+﻿namespace SystemAnalisis_CoursePaper
+{
+    static class DataSample
+    {
+        public static int size = 40;
+
+        public static double[] x1 = {
+           -0.89226, -2.964, 2.407427, -0.95317, 0.09662, -0.9001, -1.75405, -0.52284, 1.279751, 2.882271,
+           -1.53892, 0.192879, -0.41338, -1.16492, -0.93506, -0.42939, 0.504345, -1.98881, 1.907867, -0.66521,
+           -0.75723, -0.95764, -0.12167, 0.441017, 0.572774, 0.159065, -0.24437, 1.192335, 2.307802, -2.43077,
+           -0.12154, 1.607998, 2.863991, 0.448929, -0.90951, 0.895402, -1.64199, -2.81261, 0.799087, -1.47432
+        };
+
+        public static double[] x2 = {
+            -2.82594, 2.348727, 4.867938, 4.551945, 4.362961, 0.19983, 4.975569, 4.613684, 5.37971, 1.029973,
+            4.457407, 0.355628, 2.877735, 4.467752, 3.868594, 1.047429, 1.173919, 0.850284, 1.081194, 2.024751,
+            5.270139, 3.042793, 0.732759, 3.980216, 5.442886, 3.222946, 2.513169, 5.291304, 4.181383, 0.68943,
+            2.307911, 2.917626, 0.182288, 1.404388, 5.452539, 5.866652, 0.397018, 0.342916, 5.599449, 2.578642,
+        };
+
+        public static double[] x3 = {
+            2.437804, 2.288619, 0.206897, 2.863508, 1.472163, 1.184338, -0.4663, 1.045543, -0.52373, 2.478775,
+            0.305943, 1.852033, -0.05244, -0.28462, 2.528682, -0.94162, 2.859801, 2.247144, 0.06252, -0.54769,
+            1.399765, 0.569844, 0.264706, 0.563958, -0.41052, 2.717488, -0.80085, 2.556176, 0.770374, 0.507027,
+            1.664577, 0.121235, 1.374925, 2.571356, -0.94326, 2.750845, 1.091988, 0.137372, 1.870341, 0.050122,
+        };
+
+        public static double[] y = {
+            -32.2243, -3.18629, -4.63969, -2.31684, -4.00867, 2.806639, -13.1085, -6.48999, -8.79858, 12.36579,
+            -8.17719, 5.664358, -5.54466, -9.92461, -2.72403, -4.58737, 6.253611, 1.513798, 3.859126, -4.82765,
+            -7.38277, -5.08878, 1.932457, -2.99832, -10.1103, 1.848969, -5.21345, 0.498303, 0.16664, -4.23068,
+            0.2336, -1.07273, 10.82174, 6.616666, -15.2128, -2.21036, 0.729021, -3.85939, -2.65273, -6.40084
+        };
+
+        public static string baseFormula = "y = a0 + a1*x1 + a2*x2 + a3*x3";
+        public static string extendedFormula = "y = a0 + a1*x1 + a2*x2 + a3*x3 + a4*{0}";
+        public static string[] variantsNames =
+        {
+            "x1*x1",
+            "x1*x2",
+            "x1*x3",
+            "x2*x2",
+            "x2*x3",
+            "x3*x3"
+        };
+
+        public static double[,] GetSXMatrix(int start, int end)
+        {
+            var len = end - start;
+            var matrix = new double[len, 4];
+
+            for (int i = start; i < end; i++)
+            {
+                var k = i - start;
+                matrix[k, 0] = 1;
+                matrix[k, 1] = x1[i];
+                matrix[k, 2] = x2[i];
+                matrix[k, 3] = x3[i];
+            }
+
+            return matrix;
+        }
+
+        public static double[,] GetCorrectedSXMatrix(int start, int end, int variant)
+        {
+            var len = end - start;
+            var matrix = new double[len, 5];
+
+            for (int i = start; i < end; i++)
+            {
+                var k = i - start;
+                matrix[k, 0] = 1;
+                matrix[k, 1] = x1[i];
+                matrix[k, 2] = x2[i];
+                matrix[k, 3] = x3[i];
+
+                switch (variant)
+                {
+                    case 0:
+                        matrix[k, 4] = x1[i] * x1[i];
+                        break;
+                    case 1:
+                        matrix[k, 4] = x1[i] * x2[i];
+                        break;
+                    case 2:
+                        matrix[k, 4] = x1[i] * x3[i];
+                        break;
+                    case 3:
+                        matrix[k, 4] = x2[i] * x2[i];
+                        break;
+                    case 4:
+                        matrix[k, 4] = x2[i] * x3[i];
+                        break;
+                    case 5:
+                        matrix[k, 4] = x3[i] * x3[i];
+                        break;
+                }
+                
+            }
+
+            return matrix;
+        }
+        public static double[,] GetYMatrix(int start, int end)
+        {
+            var len = end - start;
+            var matrix = new double[len, 1];
+
+            for (int i = start; i < end; i++)
+            {
+                var k = i - start;
+                matrix[k, 0] = y[i];
+            }
+            return matrix;
+        }
+    }
+}
---
+++ SystemAnalisis_CoursePaper/Program.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace SystemAnalisis_CoursePaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdequate = MakeAdequateModel();
+
+            if (isAdequate == true)
+            {
+                Console.WriteLine($"Модель адекватна без корректировки!");
+                Console.ReadLine();
+                return;
+            }
+
+            // КОРРЕКТИРОВКА МОДЕЛИ (при необходимости)
+
+            for ( int variant=0; variant<6; variant++)
+            {
+                var formula = string.Format(DataSample.extendedFormula, DataSample.variantsNames[variant]);
+                Console.WriteLine($"Try find model for:\n\t{formula}\n");
+
+                // определяем начало и конец обучающей и контрольной выборок
+                var (start1, end1, start2, end2) = FindBestRange();
+
+                // получаем обучающие матрицы для x и y
+                Matrix<double> SXTrainingCorrected = DenseMatrix.OfArray(DataSample.GetCorrectedSXMatrix(start1, end1, variant));
+                Matrix<double> YTraining = DenseMatrix.OfArray(DataSample.GetYMatrix(start1, end1));
+                Console.WriteLine("Матрица y:\n" + YTraining.ToString());
+
+                // определяем параметры модели (значения коэффициентов) по формуле метода наименьших квадратов
+                Matrix<double> SXT = SXTrainingCorrected.Transpose();
+                Matrix<double> A = (SXT * SXTrainingCorrected).Inverse() * SXT * YTraining;
+
+                Console.WriteLine("Матрица модели:\n" + A.ToString());
+
+                // получаем контрольные матрицы для x и y
+                Matrix<double> SXControl = DenseMatrix.OfArray(DataSample.GetCorrectedSXMatrix(start2, end2, variant));
+                Matrix<double> YControl = DenseMatrix.OfArray(DataSample.GetYMatrix(start2, end2));
+
+                // вычисляем вектор ошибок на основе контрольной выборки
+                Matrix<double> YVector = SXControl * A;
+                Matrix<double> E = YControl - YVector;
+                
+                Console.WriteLine("Матрица ошибок:\n"+E.ToString());
+
+                // получаем J - оценку качества модели
+                double J = GetQuality(DataSample.y, YVector, start2, end2);
+
+                Console.WriteLine($"J = {J}");
+
+                // в три этапа проверяем адекватность полученной модели
+
+                // этап 1. отсутствие тренда (проверка с использованием критерия знаков)
+                // 1.1 вычисляем последовательность 0 и 1 для временного ряда
+                int[] zeroOneSeq = ZeroOneSeq(E, end2 - start2);
+
+                // вычисляем количество серий и длину наибольшей серии
+                var (sequenceQuantity, sequenceMaxLen) = GetSequenceStat(zeroOneSeq);
+                Console.WriteLine($"Количество серий = {sequenceQuantity} Максимальная длина = {sequenceMaxLen}");
+
+                // определяем наличие либо отсутствие тренда
+                bool notTrend = IsTrend(DataSample.size / 2, sequenceMaxLen, sequenceQuantity);
+                Console.WriteLine($"Не тренд = {notTrend}");
+
+                // этап 2. отсутствие автокорреляции
+                bool notAutocorllation = IsAutocorellation(DataSample.size / 2, E);
+                Console.WriteLine($"Нет автокорреляции = {notAutocorllation}");
+
+                // этап 3. нормальное распределение
+                bool normalSpread = IsNormalSpread(DataSample.size / 2, E);
+                Console.WriteLine($"Нормальное распределение = {normalSpread}");
+
+                bool isCorrectedAdequate = notTrend && notAutocorllation && normalSpread;
+                Console.WriteLine($"Откорректированная модель адекватна = {isCorrectedAdequate}");
+
+                Console.WriteLine($"Расчеты для модели:\t{formula}");
+                Console.WriteLine("\n\n\n");
+            }
+            Console.ReadLine();
+        }
+
+
+        static bool MakeAdequateModel()
+        {
+            // определяем начало и конец обучающей и контрольной выборок
+            var (start1, end1, start2, end2) = FindBestRange();
+            Console.WriteLine($"Начало и окончание обучающей выборки: {start1}, {end1}\n Начало и окончание контрольной выборки: {start2}, {end2}\n");
+
+            // получаем обучающие матрицы для x и y
+            Matrix<double> SXTraining = DenseMatrix.OfArray(DataSample.GetSXMatrix(start1, end1));
+            Matrix<double> YTraining = DenseMatrix.OfArray(DataSample.GetYMatrix(start1, end1));
+
+            Console.WriteLine("Матрица x:\n" + SXTraining.ToString());
+            Console.WriteLine("Матрица y:\n" + YTraining.ToString());
+
+            // определяем параметры модели (значения коэффициентов) по формуле метода наименьших квадратов
+            Matrix<double> SXT = SXTraining.Transpose();
+            Matrix<double> A = (SXT * SXTraining).Inverse() * SXT * YTraining;
+
+            Console.WriteLine("Матрица модели:\n" + A.ToString());
+
+            // получаем контрольные матрицы для x и y
+            Matrix<double> SXControl = DenseMatrix.OfArray(DataSample.GetSXMatrix(start2, end2));
+            Matrix<double> YControl = DenseMatrix.OfArray(DataSample.GetYMatrix(start2, end2));
+
+            // вычисляем вектор ошибок на основе контрольной выборки
+            Matrix<double> YVector = SXControl * A;
+            Matrix<double> E = YControl - YVector;
+
+            Console.WriteLine("Матрица ошибок:\n"+E.ToString());
+
+            // получаем J - оценку качества модели
+            double J = GetQuality(DataSample.y, E, start2, end2);
+
+            Console.WriteLine($"J = {J}");
+
+            // в три этапа проверяем адекватность полученной модели
+
+            // этап 1. отсутствие тренда (проверка с использованием критерия знаков)
+            // 1.1 вычисляем последовательность 0 и 1 для временного ряда
+            int[] zeroOneSeq = ZeroOneSeq(E, end2 - start2);
+
+            // вычисляем количество серий и длину наибольшей серии
+            var (sequenceQuantity, sequenceMaxLen) = GetSequenceStat(zeroOneSeq);
+
+            // определяем наличие либо отсутствие тренда
+            bool notTrend = IsTrend(DataSample.size / 2, sequenceMaxLen, sequenceQuantity);
+
+            // этап 2. отсутствие автокорреляции
+            bool notAutocorllation = IsAutocorellation(DataSample.size / 2, E);
+
+            // этап 3. нормальное распределение
+            bool normalSpread = IsNormalSpread(DataSample.size / 2, E);
+
+            bool isAdequate = notTrend && notAutocorllation && normalSpread;
+
+            return isAdequate;
+        }
+
+        // метод для разбиения выборки на контрольную и обучающую (возвращает точки начала и конца каждой из частей)
+        static (int, int, int, int) FindBestRange()
+        {
+            double avg = GetAverageY(DataSample.y);
+            double deviation1 = GetDeviationY(DataSample.y, avg, 0, DataSample.size / 2);
+            double deviation2 = GetDeviationY(DataSample.y, avg, DataSample.size / 2, DataSample.size);
+            if (deviation1 > deviation2)
+            {
+                return (
+                    0, DataSample.size / 2,
+                    DataSample.size / 2, DataSample.size);
+            }
+
+            return (
+                DataSample.size / 2, DataSample.size,
+                0, DataSample.size / 2);
+        }
+
+        // вспомогательный метод для определения среднего значения у для исходной выборки
+        static double GetAverageY(double[] y)
+        {
+            int len = y.Length;
+            double sum = 0;
+            for (int i = 0; i < len; i++)
+                sum += y[i];
+            return sum / len;
+        }
+
+        // вспомогательный метод для определения отклонения у для заданной части выборки
+        static double GetDeviationY(double[] y, double avg, int start, int end)
+        {
+            double deviation = 0;
+            for (int i = start; i < end; i++)
+            {
+                Console.WriteLine($"{y[i] - avg}");
+                deviation += Math.Abs(y[i] - avg);
+            }
+            return deviation;
+        }
+
+        // метод для определения качества модели
+        static double GetQuality(double[] y, Matrix<double> yVector, int start, int end)
+        {
+            double quadratic = 0;
+            for (int i = start; i < end; i++)
+            {
+                var k = i - start;
+                double yAbs = y[i] - yVector[k, 0];
+                quadratic += yAbs * yAbs;
+            }
+            int M = end - start;
+            double result =  quadratic / M;
+            return result;
+        }
+
+        // вспомогательный метод для генерации последовательности 0 и 1 (тренд)
+        static int[] ZeroOneSeq(Matrix<double> E, int len)
+        {
+            int[] seq = new int [len];
+
+            for (int i = 1; i < len; i++)
+            {
+                if ((E[i, 0] - E[i-1, 0]) > 0)
+                {
+                    seq[i] = 1;
+                }
+                else
+                {
+                    seq[1] = 0;
+                }
+            }
+            return seq;
+        }
+
+        // вспомогательный метод для вычисления количества серий и максимальной продолжительности серии (тренд)
+        static (int, int) GetSequenceStat(int[] seq)
+        {
+            int seqLen = seq.Length;
+            
+            int qty = 1;
+            int maxSize = 1;
+            int curSize = 1;
+            
+            for (int i = 1; i < seqLen; i++)
+            {
+                if (seq[i] != seq[i - 1])
+                {
+                    qty++;
+                    curSize = 0;
+                }
+
+                curSize += 1;
+                if (curSize > maxSize)
+                    maxSize = curSize;
+            }
+
+            return (qty, maxSize);
+        }
+
+        // метод для определения наличия тренда, возвращает true/false
+        static bool IsTrend(int sampleLen, int maxSeqSize, int seqQty)
+        {
+            bool isGoodSeqQty = seqQty > Math.Abs(Math.Floor(1 / 3 * (2 * sampleLen - 1) 
+                - 1.96 * Math.Sqrt((16 * sampleLen - 29) / 90)));
+            bool isGoodSeqSize = (maxSeqSize <= 5);
+
+            return isGoodSeqQty && isGoodSeqSize;
+        }
+
+        // метод для вычисления критерия Дарбина-Уотсона (автокорреляция)
+        static double GetDarbinWatson(int sampleLen, Matrix<double> E) 
+        {
+            double quadDiff = 0;
+            double quadSum = 0;
+
+            for (int i = 1; i < sampleLen; i++)
+            {
+                quadDiff += Math.Pow((E[i, 0] - E[i - 1, 0]), 2);
+            }
+
+            for (int i = 0; i < sampleLen; i++)
+            {
+                quadSum += Math.Pow((E[i, 0]), 2);
+            }
+
+            return quadDiff / quadSum;
+        }
+
+        // метод для определения автокорреляции 
+        static bool IsAutocorellation(int sampleLen, Matrix<double> E)
+        {
+            double d = GetDarbinWatson(sampleLen, E);
+
+            double dMin = 1.68;
+            double dMax = 4 - dMin;
+
+            return dMin <= d && d <= dMax;
+        }
+
+        static double GetEPow(Matrix<double> E, int power, int sampleLen)
+        {
+            double result = 0;
+            for (int i = 0; i < sampleLen; i++)
+            {
+                result += Math.Pow((E[i, 0]), power);
+            }
+            return result;
+        }
+
+        // метод для определения нормальности распределения
+        static bool IsNormalSpread(int sampleLen, Matrix<double> E)
+        {
+            // получаем необходимые данные для вычисления формулы
+            double quadE = GetEPow(E, 2, sampleLen);
+            double s = Math.Sqrt(quadE / (sampleLen - 1));
+
+            double tripleE = GetEPow(E, 3, sampleLen);
+            double quatrE = GetEPow(E, 4, sampleLen);
+
+            double assymetryErr = Math.Sqrt((6 * (sampleLen - 2)) / ((sampleLen + 1) * (sampleLen + 3)));
+            double excessErr = Math.Sqrt((24 * sampleLen * (sampleLen - 2) * (sampleLen - 3)) / 
+                               (Math.Pow((sampleLen - 1), 2) * (sampleLen + 3) * (sampleLen + 5)));
+
+            double assymetry = tripleE / (sampleLen * Math.Pow(s, 3));
+            double excess = quatrE / (sampleLen * Math.Pow(s, 4)) - 3;
+            Console.WriteLine($"assymetryErr = {assymetryErr}");
+            Console.WriteLine($"excessErr = {excessErr}");
+            Console.WriteLine($"assymetry = {assymetry}");
+            Console.WriteLine($"excess = {excess}");
+
+            if ((Math.Abs(assymetry) >= 2 * s) | (Math.Abs(excess + (6 / sampleLen + 1)) >= 2 * s))
+                return false;
+
+            if ((Math.Abs(assymetry) < 1.5 * s) & (Math.Abs(excess + (6 / sampleLen + 1)) < 1.5 * s))
+                return true;
+
+            return false;
+        }
+    }
+}
